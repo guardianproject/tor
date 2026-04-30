cbc8a92c9df56bb8cb8776912845121da69945f3
David Goulet <dgoulet@torproject.org>
2026-04-30 10:50:13 -0400

    cell: Off-by-one out-of-bounds read in malicious BEGIN
    A well crafted BEGIN cell could trigger an off-by-one read outside of the
    memory bounardy of a cell payload.
    
    This would be undefined behavior in C so anything can happen really on the
    network when this occurs.
    
    This adds a unit tests that makes sure the BEGIN cell's flags are not set for
    that very condition making sure our test works.
    
    Reported by Hackerone. TROVE-2026-007
    
    Fixes #41245
    
    Signed-off-by: David Goulet <dgoulet@torproject.org>

--- src/core/or/connection_edge.c
+++ src/core/or/connection_edge.c
@@ -3865,7 +3865,7 @@ begin_cell_parse(const cell_t *cell, begin_cell_t *bcell,
     *end_reason_out = END_STREAM_REASON_TORPROTOCOL;
     return -1;
   }
-  if (body + rh.length >= nul + 4)
+  if (body + rh.length > nul + 4)
     bcell->flags = ntohl(get_uint32(nul+1));
 
   return 0;
